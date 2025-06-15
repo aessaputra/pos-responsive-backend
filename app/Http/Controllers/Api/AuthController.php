8e4a3f13738e59975c2c6134265f762814489a81
Aes Saputra <aessaputra@yahoo.com>
2025-06-16 00:25:14 +0700

    Refactor authentication logic by implementing LoginRequest for validation and enhancing login/logout functionality in AuthController

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,20 +3,41 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Models\User;
+use App\Http\Requests\LoginRequest;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function login (Request $request)
+    public function login(LoginRequest $request): JsonResponse
     {
-        $loginData = $request->validate([
-            'email' => 'required|email',
-            'password' => 'required|string',
+        if (!Auth::attempt($request->only('email', 'password'))) {
+
+            throw ValidationException::withMessages([
+                'email' => [__('auth.failed')],
+            ]);
+        }
+
+        $user = $request->user();
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'message' => __('auth.success'),
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+            'user' => $user,
+        ], 200);
+    }
+
+    public function logout(Request $request): JsonResponse
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json([
+            'message' => 'You have successfully been logged out.'
         ]);
-
-        $user = User::where('email', $loginData['email'])->first();
-
-        
     }
 }
